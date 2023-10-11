c755c7e145535ce402c238312b1dc26c4b7ae1f0
xxeol2 <jhj01177@naver.com>
2023-10-11 12:03:08 +0900

    refactor: ImageStorageService단 File validation 추가

--- src/main/java/practice/s3/application/ImageStorageService.java
+++ src/main/java/practice/s3/application/ImageStorageService.java
@@ -3,24 +3,25 @@ package practice.s3.application;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import practice.s3.exception.ImageStorageException;
 
 @Service
+@RequiredArgsConstructor
 public class ImageStorageService {
+
+    private static final int MAX_IMAGE_LENGTH = 10;
 
     private final ImageStorageClient imageStorageClient;
 
     @Value("${s3.base-url}")
     private String baseUrl;
 
-    public ImageStorageService(ImageStorageClient imageStorageClient) {
-        this.imageStorageClient = imageStorageClient;
-    }
-
     public List<String> uploadFiles(MultipartFile[] imageFiles) {
+        validate(imageFiles);
         List<String> fileNames = new ArrayList<>();
         try {
             Arrays.stream(imageFiles)
@@ -30,6 +31,21 @@ public class ImageStorageService {
         } catch (ImageStorageException e) {
             fileNames.forEach(imageStorageClient::delete);
             throw e;
+        }
+    }
+
+    private void validate(MultipartFile[] imageFiles) {
+        if (imageFiles.length > MAX_IMAGE_LENGTH) {
+            throw new ImageStorageException("[File Upload 실패] 파일 수가 너무 많습니다.");
+        }
+        Arrays.stream(imageFiles)
+            .forEach(this::validateImageFile);
+    }
+
+    private void validateImageFile(MultipartFile file) {
+        String contentType = file.getContentType();
+        if (contentType == null || !contentType.startsWith("image/")) {
+            throw new ImageStorageException("[File Upload 실패] image 형식이 아닙니다.");
         }
     }
 
