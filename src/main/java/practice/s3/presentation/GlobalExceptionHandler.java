392e9ef7addb99908bc95d31c203205d7f526cf0
xxeol2 <jhj01177@naver.com>
2023-10-11 11:37:11 +0900

    refactor: 요청의 크기가 클 때 예외처리

--- src/main/java/practice/s3/presentation/GlobalExceptionHandler.java
+++ src/main/java/practice/s3/presentation/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import practice.s3.dto.ErrorResponse;
 import practice.s3.exception.ImageStorageException;
@@ -12,7 +13,7 @@ import practice.s3.exception.ImageStorageException;
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({ImageStorageException.class})
+    @ExceptionHandler({MaxUploadSizeExceededException.class, ImageStorageException.class})
     public ResponseEntity<ErrorResponse> handleBadRequest(Exception e) {
         log.warn(e.getMessage());
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
