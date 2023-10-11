d57650763891d31ef95a97824094d694ea90f2d6
xxeol2 <jhj01177@naver.com>
2023-10-11 12:02:10 +0900

    refactor: ImageStorageService단 File validation 추가

--- src/main/java/practice/s3/application/ImageStorageService.java
+++ src/main/java/practice/s3/application/ImageStorageService.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -11,19 +12,19 @@ import org.springframework.web.multipart.MultipartFile;
 import practice.s3.exception.ImageStorageException;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class ImageStorageService {
+
+    private static final int MAX_IMAGE_LENGTH = 10;
 
     private final ImageStorageClient imageStorageClient;
 
     @Value("${s3.base-url}")
     private String baseUrl;
 
-    public ImageStorageService(ImageStorageClient imageStorageClient) {
-        this.imageStorageClient = imageStorageClient;
-    }
-
     public List<String> uploadFiles(MultipartFile[] imageFiles) {
+        validate(imageFiles);
         AtomicReference<ImageStorageException> exceptionHolder = new AtomicReference<>();
         List<String> fileNames = uploadFilesInParallel(imageFiles, exceptionHolder);
 
@@ -33,6 +34,21 @@ public class ImageStorageService {
         }
 
         return convertToUrl(fileNames);
+    }
+
+    private void validate(MultipartFile[] imageFiles) {
+        if (imageFiles.length > MAX_IMAGE_LENGTH) {
+            throw new ImageStorageException("[File Upload 실패] 파일 수가 너무 많습니다.");
+        }
+        Arrays.stream(imageFiles)
+            .forEach(this::validateImageFile);
+    }
+
+    private void validateImageFile(MultipartFile file) {
+        String contentType = file.getContentType();
+        if (contentType == null || !contentType.startsWith("image/")) {
+            throw new ImageStorageException("[File Upload 실패] image 형식이 아닙니다.");
+        }
     }
 
     private List<String> uploadFilesInParallel(MultipartFile[] imageFiles,
